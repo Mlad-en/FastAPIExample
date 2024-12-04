61c79f2b57de405aba5ab072b19e3d78a6303386
Mlad-en <74152316+Mlad-en@users.noreply.github.com>
2024-12-04 11:25:04 +0200

    added structured logging to the project.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 from src.routers import entities
 from src.db.session import create_all_tables
+from src.middleware import logging
 
 
 @asynccontextmanager
@@ -13,3 +15,5 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=entities.router)
+
+app.add_middleware(BaseHTTPMiddleware, dispatch=logging.log_error_response)
---
+++ src/middleware/logging.py
@@ -0,0 +1,44 @@
+import json
+import sys
+
+from fastapi import Request
+from loguru import logger
+from starlette.concurrency import iterate_in_threadpool
+
+
+def serialize(record):
+    subset = {
+        "timestamp": record["time"].isoformat(),
+        "message": record["message"],
+        "status": record["extra"]["response_code"],
+        "response_body": record["extra"]["response_body"],
+        "request_url": record["extra"]["request_url"],
+        "request_headers": record["extra"]["request_headers"],
+        "request_params": record["extra"]["request_params"],
+    }
+    return subset
+
+
+def patching(record):
+    record["extra"]["serialized"] = serialize(record)
+
+
+logger = logger.patch(patching)
+logger.add(sys.stderr, format="{extra[serialized]}")
+
+
+async def log_error_response(request: Request, call_next):
+    response = await call_next(request)
+
+    if response.status_code >= 400:
+        response_body = [chunk async for chunk in response.body_iterator]
+        response.body_iterator = iterate_in_threadpool(iter(response_body))
+        logger.bind(
+            response_code=response.status_code,
+            response_body=response_body[0].decode(),
+            request_url=str(request.url),
+            request_params=dict(request.query_params),
+            request_headers=dict(request.headers),
+        ).error("Request failed")
+
+    return response
